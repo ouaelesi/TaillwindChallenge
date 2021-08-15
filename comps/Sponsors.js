803bdebf136ce44c9ad1beb3e01c2485c4d0d47f
ouaelesi <ouael2019esi@gmail.com>
2021-08-15 15:55:30 -0700

    add: finalv2

--- comps/Sponsors.js
+++ comps/Sponsors.js
@@ -1,6 +1,6 @@
 const Sponsors = () => {
     return ( 
-        <div className="my-20 grid grid-cols-5 lg:px-20">
+        <div className="my-20 grid md:grid-cols-5 grid-cols-3 lg:px-20 gap-4 px-3">
             <div ><img src="/assets/spon1.PNG" className="block mx-auto"></img></div>
             <div ><img src="/assets/spon2.PNG" className="block mx-auto"></img></div>
             <div ><img src="/assets/spon3.PNG" className="block mx-auto"></img></div>
